8c63e8e89a1053126b33c2633964cc94b6c43e4f
Metriximor <metriximor@gmail.com>
2024-10-08 22:39:21 +0100

    Add Camera Zooming in and out

--- src/camera/rts_camera_component.gd
+++ src/camera/rts_camera_component.gd
@@ -4,6 +4,7 @@ class_name RTSCameraComponent
 @export var camera: Camera3D
 @export_category("Component Parameters")
 @export var camera_speed := 25.0
+@export var zoom_speed := 50.0
 
 var _current_pan_speed := 0.0
 
@@ -13,6 +14,16 @@ func _process(delta: float) -> void:
 	var camera_pan := input_dir * camera_speed * delta
 	camera.global_position += Vector3(camera_pan.x, 0, camera_pan.y)
 	
-	var zoom := Input.get_axis("zoom_out", "zoom_in")
-	print(Input.is_action_pressed("zoom_in"))
-	camera.global_position.y += zoom * camera_speed 
+	var zoomed_out := Input.is_action_just_pressed("zoom_out")
+	var zoomed_in := Input.is_action_just_released("zoom_in")
+	camera.global_position.y += _find_zoom_axis() * zoom_speed  * delta
+
+
+func _find_zoom_axis() -> float:
+	var zoom_in := 0.0
+	var zoom_out := 0.0
+	if Input.is_action_just_released("zoom_out"):
+		zoom_out = 1.0
+	if Input.is_action_just_released("zoom_in"):
+		zoom_in = 1.0
+	return zoom_in - zoom_out
