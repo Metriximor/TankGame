b86684ade779ebeb8336046812476545879a529a
Metriximor <metriximor@gmail.com>
2024-10-08 22:56:29 +0100

    Add acceleration/momentum to movement component

--- src/unit/movement_component.gd
+++ src/unit/movement_component.gd
@@ -3,16 +3,19 @@ class_name MovementComponent
 
 @export var unit: Unit
 @export_category("Component Parameters")
-@export var speed: float = 5.0
-@export var acceleration: float = 2.0
+@export var speed: float = 10.0
+@export var acceleration: float = 5.0
 
 
 func _physics_process(delta: float) -> void:
-	unit.velocity = calculate_velocity(unit.global_position)
+	# TODO i'm not sure if normalizing here is a good idea, feels like I'm throwing
+	# away possible useful engine C++ code only to roll my own, please investigate further
+	var direction := calculate_direction(unit.global_position).normalized()
+	unit.velocity = unit.velocity.lerp(direction * speed, acceleration * delta)
 	unit.move_and_slide()
 
 
-func calculate_velocity(position: Vector3) -> Vector3:
+func calculate_direction(position: Vector3) -> Vector3:
 	if self.is_navigation_finished():
 		return Vector3.ZERO
 	var direction = position.direction_to(self.get_next_path_position())
